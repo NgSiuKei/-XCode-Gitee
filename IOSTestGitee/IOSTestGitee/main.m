118bab545d920c680c1fe977aa613ad9b29fb0ac
NgSiuKei <835474695@qq.com>
2022-03-01 10:23:01 +0800

    添加IOS测试项目

---
+++ IOSTestGitee/IOSTestGitee/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IOSTestGitee
+//
+//  Created by yoyo on 2022/3/1.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Or Eat Shit.");
+    }
+    return 0;
+}
